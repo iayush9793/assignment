f77a444bccc3a3dc3c8b13372a699d58c4061d66
iayush9793 <94067738+iayush9793@users.noreply.github.com>
2022-01-15 21:49:59 +0530

    Add files via upload

---
+++ jan 12/like.js
@@ -0,0 +1,13 @@
+var a=0;
+var b=0;
+
+function like(){
+  
+   a=a+1;
+    document.getElementById("like").innerText=a;
+}
+function dislike(){
+  
+    b=b-1;
+     document.getElementById("dislike").innerText=b;
+ }
