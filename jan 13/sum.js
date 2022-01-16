6d1b904e4e65dd81579514fe01feb6de1d55b8f8
iayush9793 <94067738+iayush9793@users.noreply.github.com>
2022-01-16 17:28:12 +0530

    Add files via upload

---
+++ jan 13/sum.js
@@ -0,0 +1,27 @@
+document.write('<h3>Matrix</31>')
+
+var matrix = [[9,7,1,4],
+              [5,7,3,4],
+              [2,4,6,5],
+              [2,5,3,5]];
+var n = matrix.length
+var sum =0;
+for(let i =0;i<n;i++){
+    document.write('<br>');
+
+    for(let j =0;j<n;j++){
+        document.write(matrix[i][j]);
+
+        document.write(" ");
+
+        if(i == j){
+            sum = sum+matrix[i][j];
+        }
+    }
+}
+
+document.write('<br>')
+
+document.write('<h4>Sum of daigonal elements</h4>')
+
+document.write(sum);
