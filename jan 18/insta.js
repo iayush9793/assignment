9311343172e9b6f476539c52fedea2fd389242d0
iayush9793 <94067738+iayush9793@users.noreply.github.com>
2022-02-05 14:18:01 +0530

    Update insta.js

--- jan 18/insta.js
+++ jan 18/insta.js
@@ -1,1 +1,12 @@
-var pic = document.getElementById('nav2>img')
+var pic = document.getElementsByTagName('span')
+span.addEventListener('click' , reaction)
+var count = 0;
+function reaction(){
+count++;
+if(count % 2 == 0){
+    span.style.display="flex";
+}
+else{
+    span.style.display="flex";
+}
+}
